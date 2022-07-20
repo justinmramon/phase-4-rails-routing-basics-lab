303dd5a091c4f0e90f18cd562b7ad404bebf61bc
justinmramon <justinmramon@gmail.com>
2022-07-19 22:44:11 -0500

    commit tree 24f5eafc50376621ddd8561692573ab69431af2d

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        grades = Student.all.grades
+        render json: grades
+    end
+end
